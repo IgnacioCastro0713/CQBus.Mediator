c8e1c94d6882605e8f3c574edd4820c163cc9de8
IgnacioCastro0713 <ignacio.g3@hotmail.com>
2025-07-13 22:08:34 -0600

    remove comments

--- samples/API/Program.cs
+++ samples/API/Program.cs
@@ -4,11 +4,7 @@ using CQBus.Mediator;
 using CQBus.Mediator.Pipelines;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMediator(cfg =>
@@ -26,7 +22,6 @@ builder.Services.AddScoped<IWeatherService, WeatherService>();
 
 WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
